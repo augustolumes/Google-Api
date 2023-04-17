cc56cc6b9f601191cf17ecb24f19ca7ac6476446
augustolumes <82983177+augustolumes@users.noreply.github.com>
2023-04-17 17:51:46 -0300

    Add files via upload

---
+++ criarEvento.js
@@ -0,0 +1,7 @@
+// Refer to the JavaScript quickstart on how to setup the environment:
+// https://developers.google.com/calendar/quickstart/js
+// Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
+// stored credentials.
+object.addEventListener("CE", criarEvento());
+
+}
